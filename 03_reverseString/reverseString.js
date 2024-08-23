b5fcc11c22e51ac3de85573fac20ccaffffd7240
Jacob <jvwyatt7@gmail.com>
2024-08-22 21:28:22 -0500

    step 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  const char = text.split("");
+  const reversed = char.reverse();
+  const bWord = reversed.toString().replace(/,/g, "");
+  return bWord;
 };
+
+console.log(reverseString("hello"));
+
+// console.log(word.toString().replace(/,/g, ""));
 
 // Do not edit below this line
 module.exports = reverseString;
